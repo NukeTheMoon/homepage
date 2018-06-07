3d25d098dcef540c67a2f7f96c9f2ef933e34fd0
Bartosz Jędrasik <bartosz.jedrasik@perfectdashboard.com>
2018-06-07 22:28:15 +0200

    polish

--- webpack.config.js
+++ webpack.config.js
@@ -44,6 +44,15 @@ module.exports = {
                     }
 
                 }
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {}
+                    }
+                ]
             }
         ]
     },
